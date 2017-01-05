74e5d8ea7a5dd5d755f1ac2048d3779ddbf8ab90
Julio Torres <jtorres990@gmail.com>
2017-01-05 12:58:00 -0500

    profile

--- controllers/profile/profile.go
+++ controllers/profile/profile.go
@@ -4,8 +4,6 @@ import (
 	"encoding/json"
 	"net/http"
 
-	"time"
-
 	"../../helper"
 	"../../models"
 	"../../ws"
@@ -87,14 +85,332 @@ func updateNacimientoMes(w http.ResponseWriter, r *http.Request, session *models
 	w.WriteHeader(http.StatusNoContent)
 }
 
+func updateNacimientoAno(w http.ResponseWriter, r *http.Request, session *models.User, hub *ws.Hub, perfil models.Profile) {
+	var err error
+	if !helper.IsValidPermision(r.PostFormValue("permiso_nacimiento_ano")) {
+		w.WriteHeader(http.StatusNotAcceptable)
+		return
+	}
+	perfil.NacimientoAno = r.PostFormValue("nacimiento_ano")
+	perfil.PermisoNacimientoAno = r.PostFormValue("permiso_nacimiento_ano")
+	if perfil.Id == 0 {
+		perfil.Usuario = session.Usuario
+		_, err = models.Add(perfil)
+	} else {
+		_, err = models.Update(perfil.Id, perfil)
+	}
+	if err != nil {
+		w.WriteHeader(http.StatusNotAcceptable)
+		return
+	}
+	w.WriteHeader(http.StatusNoContent)
+}
+
+func updateNacimientoSexo(w http.ResponseWriter, r *http.Request, session *models.User, hub *ws.Hub, perfil models.Profile) {
+	var err error
+	if !helper.IsValidPermision(r.PostFormValue("permiso_sexo")) {
+		w.WriteHeader(http.StatusNotAcceptable)
+		return
+	}
+	perfil.Sexo = r.PostFormValue("sexo")
+	perfil.PermisoSexo = r.PostFormValue("permiso_sexo")
+	if perfil.Id == 0 {
+		perfil.Usuario = session.Usuario
+		_, err = models.Add(perfil)
+	} else {
+		_, err = models.Update(perfil.Id, perfil)
+	}
+	if err != nil {
+		w.WriteHeader(http.StatusNotAcceptable)
+		return
+	}
+	w.WriteHeader(http.StatusNoContent)
+}
+
+func updateNacimientoPais(w http.ResponseWriter, r *http.Request, session *models.User, hub *ws.Hub, perfil models.Profile) {
+	var err error
+	if !helper.IsValidPermision(r.PostFormValue("permiso_nacimiento_pais")) {
+		w.WriteHeader(http.StatusNotAcceptable)
+		return
+	}
+	perfil.NacimientoPais = r.PostFormValue("nacimiento_pais")
+	perfil.PermisoNacimientoPais = r.PostFormValue("permiso_nacimiento_pais")
+	if perfil.Id == 0 {
+		perfil.Usuario = session.Usuario
+		_, err = models.Add(perfil)
+	} else {
+		_, err = models.Update(perfil.Id, perfil)
+	}
+	if err != nil {
+		println(err.Error())
+		w.WriteHeader(http.StatusNotAcceptable)
+		return
+	}
+	w.WriteHeader(http.StatusNoContent)
+}
+
+func updateNacimientoCiudad(w http.ResponseWriter, r *http.Request, session *models.User, hub *ws.Hub, perfil models.Profile) {
+	var err error
+	if !helper.IsValidPermision(r.PostFormValue("permiso_nacimiento_ciudad")) {
+		w.WriteHeader(http.StatusNotAcceptable)
+		return
+	}
+	perfil.NacimientoCiudad = r.PostFormValue("nacimiento_ciudad")
+	perfil.PermisoNacimientoCiudad = r.PostFormValue("permiso_nacimiento_ciudad")
+	if perfil.Id == 0 {
+		perfil.Usuario = session.Usuario
+		_, err = models.Add(perfil)
+	} else {
+		_, err = models.Update(perfil.Id, perfil)
+	}
+	if err != nil {
+		w.WriteHeader(http.StatusNotAcceptable)
+		return
+	}
+	w.WriteHeader(http.StatusNoContent)
+}
+
+func updateResidenciaPais(w http.ResponseWriter, r *http.Request, session *models.User, hub *ws.Hub, perfil models.Profile) {
+	var err error
+	if !helper.IsValidPermision(r.PostFormValue("permiso_residencia_pais")) {
+		w.WriteHeader(http.StatusNotAcceptable)
+		return
+	}
+	perfil.ResidenciaPais = r.PostFormValue("residencia_pais")
+	perfil.PermisoResidenciaPais = r.PostFormValue("permiso_residencia_pais")
+	if perfil.Id == 0 {
+		perfil.Usuario = session.Usuario
+		_, err = models.Add(perfil)
+	} else {
+		_, err = models.Update(perfil.Id, perfil)
+	}
+	if err != nil {
+		w.WriteHeader(http.StatusNotAcceptable)
+		return
+	}
+	w.WriteHeader(http.StatusNoContent)
+}
+
+func updateResidenciaCiudad(w http.ResponseWriter, r *http.Request, session *models.User, hub *ws.Hub, perfil models.Profile) {
+	var err error
+	if !helper.IsValidPermision(r.PostFormValue("permiso_residencia_ciudad")) {
+		w.WriteHeader(http.StatusNotAcceptable)
+		return
+	}
+	perfil.ResidenciaCiudad = r.PostFormValue("residencia_ciudad")
+	perfil.PermisoResidenciaCiudad = r.PostFormValue("permiso_residencia_ciudad")
+	if perfil.Id == 0 {
+		perfil.Usuario = session.Usuario
+		_, err = models.Add(perfil)
+	} else {
+		_, err = models.Update(perfil.Id, perfil)
+	}
+	if err != nil {
+		w.WriteHeader(http.StatusNotAcceptable)
+		return
+	}
+	w.WriteHeader(http.StatusNoContent)
+}
+
+func updateDireccion(w http.ResponseWriter, r *http.Request, session *models.User, hub *ws.Hub, perfil models.Profile) {
+	var err error
+	if !helper.IsValidPermision(r.PostFormValue("permiso_direccion")) {
+		w.WriteHeader(http.StatusNotAcceptable)
+		return
+	}
+	perfil.Direccion = r.PostFormValue("direccion")
+	perfil.PermisoDireccion = r.PostFormValue("permiso_direccion")
+	if perfil.Id == 0 {
+		perfil.Usuario = session.Usuario
+		_, err = models.Add(perfil)
+	} else {
+		_, err = models.Update(perfil.Id, perfil)
+	}
+	if err != nil {
+		w.WriteHeader(http.StatusNotAcceptable)
+		return
+	}
+	w.WriteHeader(http.StatusNoContent)
+}
+
+func updateTelefono(w http.ResponseWriter, r *http.Request, session *models.User, hub *ws.Hub, perfil models.Profile) {
+	var err error
+	if !helper.IsValidPermision(r.PostFormValue("permiso_telefono")) {
+		w.WriteHeader(http.StatusNotAcceptable)
+		return
+	}
+	perfil.Telefono = r.PostFormValue("telefono")
+	perfil.PermisoTelefono = r.PostFormValue("permiso_telefono")
+	if perfil.Id == 0 {
+		perfil.Usuario = session.Usuario
+		_, err = models.Add(perfil)
+	} else {
+		_, err = models.Update(perfil.Id, perfil)
+	}
+	if err != nil {
+		w.WriteHeader(http.StatusNotAcceptable)
+		return
+	}
+	w.WriteHeader(http.StatusNoContent)
+}
+
+func updateCelular(w http.ResponseWriter, r *http.Request, session *models.User, hub *ws.Hub, perfil models.Profile) {
+	var err error
+	if !helper.IsValidPermision(r.PostFormValue("permiso_celular")) {
+		w.WriteHeader(http.StatusNotAcceptable)
+		return
+	}
+	perfil.Celular = r.PostFormValue("celular")
+	perfil.PermisoCelular = r.PostFormValue("permiso_celular")
+	if perfil.Id == 0 {
+		perfil.Usuario = session.Usuario
+		_, err = models.Add(perfil)
+	} else {
+		_, err = models.Update(perfil.Id, perfil)
+	}
+	if err != nil {
+		w.WriteHeader(http.StatusNotAcceptable)
+		return
+	}
+	w.WriteHeader(http.StatusNoContent)
+}
+
+func updatePersonalidad(w http.ResponseWriter, r *http.Request, session *models.User, hub *ws.Hub, perfil models.Profile) {
+	var err error
+	if !helper.IsValidPermision(r.PostFormValue("permiso_personalidad")) {
+		w.WriteHeader(http.StatusNotAcceptable)
+		return
+	}
+	perfil.Personalidad = r.PostFormValue("personalidad")
+	perfil.PermisoPersonalidad = r.PostFormValue("permiso_personalidad")
+	if perfil.Id == 0 {
+		perfil.Usuario = session.Usuario
+		_, err = models.Add(perfil)
+	} else {
+		_, err = models.Update(perfil.Id, perfil)
+	}
+	if err != nil {
+		w.WriteHeader(http.StatusNotAcceptable)
+		return
+	}
+	w.WriteHeader(http.StatusNoContent)
+}
+
+func updateIntereses(w http.ResponseWriter, r *http.Request, session *models.User, hub *ws.Hub, perfil models.Profile) {
+	var err error
+	if !helper.IsValidPermision(r.PostFormValue("permiso_intereses")) {
+		w.WriteHeader(http.StatusNotAcceptable)
+		return
+	}
+	perfil.Intereses = r.PostFormValue("intereses")
+	perfil.PermisoIntereses = r.PostFormValue("permiso_intereses")
+	if perfil.Id == 0 {
+		perfil.Usuario = session.Usuario
+		_, err = models.Add(perfil)
+	} else {
+		_, err = models.Update(perfil.Id, perfil)
+	}
+	if err != nil {
+		w.WriteHeader(http.StatusNotAcceptable)
+		return
+	}
+	w.WriteHeader(http.StatusNoContent)
+}
+
+func updateMusica(w http.ResponseWriter, r *http.Request, session *models.User, hub *ws.Hub, perfil models.Profile) {
+	var err error
+	if !helper.IsValidPermision(r.PostFormValue("permiso_musica")) {
+		w.WriteHeader(http.StatusNotAcceptable)
+		return
+	}
+	perfil.Musica = r.PostFormValue("musica")
+	perfil.PermisoMusica = r.PostFormValue("permiso_musica")
+	if perfil.Id == 0 {
+		perfil.Usuario = session.Usuario
+		_, err = models.Add(perfil)
+	} else {
+		_, err = models.Update(perfil.Id, perfil)
+	}
+	if err != nil {
+		w.WriteHeader(http.StatusNotAcceptable)
+		return
+	}
+	w.WriteHeader(http.StatusNoContent)
+}
+
+func updateSeries(w http.ResponseWriter, r *http.Request, session *models.User, hub *ws.Hub, perfil models.Profile) {
+	var err error
+	if !helper.IsValidPermision(r.PostFormValue("permiso_series")) {
+		w.WriteHeader(http.StatusNotAcceptable)
+		return
+	}
+	perfil.Series = r.PostFormValue("series")
+	perfil.PermisoSeries = r.PostFormValue("permiso_series")
+	if perfil.Id == 0 {
+		perfil.Usuario = session.Usuario
+		_, err = models.Add(perfil)
+	} else {
+		_, err = models.Update(perfil.Id, perfil)
+	}
+	if err != nil {
+		w.WriteHeader(http.StatusNotAcceptable)
+		return
+	}
+	w.WriteHeader(http.StatusNoContent)
+}
+
+func updateCreenciasReligiosas(w http.ResponseWriter, r *http.Request, session *models.User, hub *ws.Hub, perfil models.Profile) {
+	var err error
+	if !helper.IsValidPermision(r.PostFormValue("permiso_creencias_religiosas")) {
+		w.WriteHeader(http.StatusNotAcceptable)
+		return
+	}
+	perfil.CreenciasReligiosas = r.PostFormValue("creencias_religiosas")
+	perfil.PermisoCreenciasReligiosas = r.PostFormValue("permiso_creencias_religiosas")
+	if perfil.Id == 0 {
+		perfil.Usuario = session.Usuario
+		_, err = models.Add(perfil)
+	} else {
+		_, err = models.Update(perfil.Id, perfil)
+	}
+	if err != nil {
+		w.WriteHeader(http.StatusNotAcceptable)
+		return
+	}
+	w.WriteHeader(http.StatusNoContent)
+}
+
+func updateCreenciasPoliticas(w http.ResponseWriter, r *http.Request, session *models.User, hub *ws.Hub, perfil models.Profile) {
+	var err error
+	if !helper.IsValidPermision(r.PostFormValue("permiso_creencias_politicas")) {
+		w.WriteHeader(http.StatusNotAcceptable)
+		return
+	}
+	perfil.CreenciasPoliticas = r.PostFormValue("creencias_politicas")
+	perfil.PermisoCreenciasPoliticas = r.PostFormValue("permiso_creencias_politicas")
+	if perfil.Id == 0 {
+		perfil.Usuario = session.Usuario
+		_, err = models.Add(perfil)
+	} else {
+		_, err = models.Update(perfil.Id, perfil)
+	}
+	if err != nil {
+		w.WriteHeader(http.StatusNotAcceptable)
+		return
+	}
+	w.WriteHeader(http.StatusNoContent)
+}
+
 // Update actualiza los datos del perfil
 func Update(w http.ResponseWriter, r *http.Request, session *models.User, hub *ws.Hub) {
 	var perfil models.Profile = getProfile(session)
-	time.Sleep(3 * time.Second)
+	//time.Sleep(3 * time.Second)
 	if r.PostFormValue("nombres") != "" && r.PostFormValue("apellidos") != "" {
 		updateProfile(w, r, session, hub)
 		return
 	}
+
+	println(r.PostFormValue("permiso_residencia_pais"))
 	if r.PostFormValue("permiso_email") != "" && r.PostFormValue("email") != "" {
 		updateEmail(w, r, session, hub, perfil)
 		return
@@ -102,6 +418,79 @@ func Update(w http.ResponseWriter, r *http.Request, session *models.User, hub *w
 	if r.PostFormValue("permiso_nacimiento_dia") != "" && r.PostFormValue("nacimiento_dia") != "" &&
 		r.PostFormValue("nacimiento_mes") != "" {
 		updateNacimientoMes(w, r, session, hub, perfil)
+		return
+	}
+	if r.PostFormValue("permiso_nacimiento_ano") != "" && r.PostFormValue("nacimiento_ano") != "" {
+		updateNacimientoAno(w, r, session, hub, perfil)
+		return
+	}
+	if r.PostFormValue("permiso_sexo") != "" && r.PostFormValue("sexo") != "" {
+		updateNacimientoSexo(w, r, session, hub, perfil)
+		return
+	}
+
+	if r.PostFormValue("permiso_nacimiento_pais") != "" && r.PostFormValue("nacimiento_pais") != "" {
+		updateNacimientoPais(w, r, session, hub, perfil)
+		return
+	}
+
+	if r.PostFormValue("permiso_nacimiento_ciudad") != "" && r.PostFormValue("nacimiento_ciudad") != "" {
+		updateNacimientoCiudad(w, r, session, hub, perfil)
+		return
+	}
+
+	if r.PostFormValue("permiso_residencia_pais") != "" && r.PostFormValue("residencia_pais") != "" {
+		updateResidenciaPais(w, r, session, hub, perfil)
+		return
+	}
+
+	if r.PostFormValue("permiso_residencia_ciudad") != "" && r.PostFormValue("residencia_ciudad") != "" {
+		updateResidenciaCiudad(w, r, session, hub, perfil)
+		return
+	}
+
+	if r.PostFormValue("permiso_direccion") != "" && r.PostFormValue("direccion") != "" {
+		updateDireccion(w, r, session, hub, perfil)
+		return
+	}
+
+	if r.PostFormValue("permiso_telefono") != "" && r.PostFormValue("telefono") != "" {
+		updateTelefono(w, r, session, hub, perfil)
+		return
+	}
+
+	if r.PostFormValue("permiso_celular") != "" && r.PostFormValue("celular") != "" {
+		updateCelular(w, r, session, hub, perfil)
+		return
+	}
+
+	if r.PostFormValue("permiso_personalidad") != "" && r.PostFormValue("personalidad") != "" {
+		updatePersonalidad(w, r, session, hub, perfil)
+		return
+	}
+
+	if r.PostFormValue("permiso_intereses") != "" && r.PostFormValue("intereses") != "" {
+		updateIntereses(w, r, session, hub, perfil)
+		return
+	}
+
+	if r.PostFormValue("permiso_series") != "" && r.PostFormValue("series") != "" {
+		updateSeries(w, r, session, hub, perfil)
+		return
+	}
+
+	if r.PostFormValue("permiso_musica") != "" && r.PostFormValue("musica") != "" {
+		updateMusica(w, r, session, hub, perfil)
+		return
+	}
+
+	if r.PostFormValue("permiso_creencias_religiosas") != "" && r.PostFormValue("creencias_religiosas") != "" {
+		updateCreenciasReligiosas(w, r, session, hub, perfil)
+		return
+	}
+
+	if r.PostFormValue("permiso_creencias_politicas") != "" && r.PostFormValue("creencias_politicas") != "" {
+		updateCreenciasPoliticas(w, r, session, hub, perfil)
 		return
 	}
 }
--- models/profile.go
+++ models/profile.go
@@ -10,7 +10,39 @@ type Profile struct {
 	PermisoEmail         string `xorm:"varchar(20)" json:"permiso_email"`
 	NacimientoDia        string `xorm:"varchar(2)" json:"nacimiento_dia"`
 	NacimientoMes        string `xorm:"varchar(2)" json:"nacimiento_mes"`
-	PermisoNacimientoDia string `xorm:"varchar(20)" json:"permiso_email"`
+	PermisoNacimientoDia string `xorm:"varchar(20)" json:"permiso_nacimiento_dia"`
+	NacimientoAno        string `xorm:"varchar(4)" json:"nacimiento_ano"`
+	PermisoNacimientoAno string `xorm:"varchar(20)" json:"permiso_nacimiento_ano"`
+	Sexo                 string `xorm:"varchar(4)" json:"sexo"`
+	PermisoSexo          string `xorm:"varchar(20)" json:"permiso_sexo"`
+
+	NacimientoPais          string `xorm:"varchar(200)" json:"nacimiento_pais"`
+	PermisoNacimientoPais   string `xorm:"varchar(20)" json:"permiso_nacimiento_pais"`
+	NacimientoCiudad        string `xorm:"varchar(200)" json:"nacimiento_ciudad"`
+	PermisoNacimientoCiudad string `xorm:"varchar(20)" json:"permiso_nacimiento_ciudad"`
+	ResidenciaPais          string `xorm:"varchar(200)" json:"residencia_pais"`
+	PermisoResidenciaPais   string `xorm:"varchar(20)" json:"permiso_residencia_pais"`
+	ResidenciaCiudad        string `xorm:"varchar(200)" json:"residencia_ciudad"`
+	PermisoResidenciaCiudad string `xorm:"varchar(20)" json:"permiso_residencia_ciudad"`
+	Direccion               string `xorm:"varchar(20)" json:"direccion"`
+	PermisoDireccion        string `xorm:"varchar(20)" json:"permiso_direccion"`
+	Telefono                string `xorm:"varchar(20)" json:"telefono"`
+	PermisoTelefono         string `xorm:"varchar(20)" json:"permiso_telefono"`
+	Celular                 string `xorm:"varchar(20)" json:"celular"`
+	PermisoCelular          string `xorm:"varchar(20)" json:"permiso_celular"`
+
+	Personalidad               string `xorm:"text" json:"personalidad"`
+	PermisoPersonalidad        string `xorm:"varchar(20)" json:"permiso_personalidad"`
+	Intereses                  string `xorm:"text" json:"intereses"`
+	PermisoIntereses           string `xorm:"varchar(20)" json:"permiso_intereses"`
+	Series                     string `xorm:"text" json:"series"`
+	PermisoSeries              string `xorm:"varchar(20)" json:"permiso_series"`
+	Musica                     string `xorm:"text" json:"musica"`
+	PermisoMusica              string `xorm:"varchar(20)" json:"permiso_musica"`
+	CreenciasReligiosas        string `xorm:"text" json:"creencias_religiosas"`
+	PermisoCreenciasReligiosas string `xorm:"varchar(20)" json:"permiso_creencias_religiosas"`
+	CreenciasPoliticas         string `xorm:"text" json:"creencias_politicas"`
+	PermisoCreenciasPoliticas  string `xorm:"varchar(20)" json:"permiso_creencias_politicas"`
 
 	CreateAt time.Time `xorm:"created" json:"create_at"`
 	UpdateAt time.Time `xorm:"updated" json:"update_at"`
