ab71437b613ad436a84bedcd2408126936b51cc6
Julio Torres <jtorres990@gmail.com>
2017-01-12 14:15:14 -0500

    friend query

--- controllers/friends/friends.go
+++ controllers/friends/friends.go
@@ -9,10 +9,74 @@ import (
 	"../../models"
 	"../../ws"
 	"../responses"
+	"errors"
 )
 
-// ListFriends listado de amigos o personas con las que se puede chatear
+func getFriends(session *models.User) ([]responses.Friend, error) {
+	var defecto = make([]responses.Friend, 0)
+	var relaciones = make([]models.Relacion, 0)
+	var users = make([]models.User, 0)
+	var orm = models.GetXORM()
+	var str string = "usuario_solicita = ? or usuario_solicitado = ?"
+	if err := orm.Where(str, session.Usuario, session.Usuario).Find(&relaciones); err != nil {
+		return defecto, errors.New("Error desconocido")
+	}
+	var data string = ""
+	for _, el := range relaciones {
+		if el.UsuarioSolicitado == session.Usuario {
+			data += "\"" + el.UsuarioSolicita + "\", "
+		} else {
+			data += "\"" + el.UsuarioSolicitado + "\", "
+		}
+	}
+	data = data[0:len(data)-2]
+	str = "Usuario in (" + data + ")"
+	if err := orm.Where(str).Find(&users); err != nil {
+		return defecto, errors.New("Error desconocido")
+	}
+	return listUserToListFriends(users, relaciones), nil
+}
+
+func listUserToListFriends(users []models.User, relacion []models.Relacion) []responses.Friend {
+	var lengthUsers = len(users)
+	var lengthRelacion = len(relacion)
+	list := make([]responses.Friend, lengthUsers)
+	for i := 0; i < lengthUsers; i++ {
+		list[i] = responses.Friend{
+			Usuario:    users[i].Usuario,
+			Nombres:    users[i].Nombres,
+			Apellidos:  users[i].Apellidos,
+			Estado:     "",
+			Registrado: users[i].CreateAt,
+		}
+		for j := 0; j < lengthRelacion; j++ {
+			solicita := relacion[j].UsuarioSolicita
+			solicitado := relacion[j].UsuarioSolicitado
+			if users[i].Usuario == solicita || users[i].Usuario == solicitado {
+				if relacion[j].EstadoRelacion == models.EstadoSolicitado {
+					list[i].Estado = "Solicitado"
+				} else {
+					list[i].Estado = "Amigos"
+				}
+			}
+		}
+	}
+	return list
+}
+
+//ListFriends listado de amigos o personas con las que se puede chatear
 func ListFriends(w http.ResponseWriter, r *http.Request, session *models.User, hub *ws.Hub) {
+	var friends, _ = getFriends(session)
+	respuesta, _ := json.Marshal(responses.ListFriends{
+		Success: true,
+		Data:    friends,
+	})
+	w.WriteHeader(http.StatusOK)
+	w.Write(respuesta)
+}
+
+// FindUser Busqueda de personas
+func FindUser(w http.ResponseWriter, r *http.Request, session *models.User, hub *ws.Hub) {
 	var users = make([]models.User, 0)
 	var relacion = make([]models.Relacion, 0)
 	var orm = models.GetXORM()
@@ -79,5 +143,28 @@ func ListFriends(w http.ResponseWriter, r *http.Request, session *models.User, h
 
 // Add agregar amigo
 func Add(w http.ResponseWriter, r *http.Request, session *models.User, hub *ws.Hub) {
-	w.WriteHeader(http.StatusOK)
+	var usuario = r.PostFormValue("user")
+	if (usuario == "") {
+		w.WriteHeader(http.StatusNotAcceptable)
+		return
+	}
+	var relaciones = make([]models.Relacion, 0)
+	var orm = models.GetXORM()
+	var str string = "(usuario_solicita = ? and usuario_solicitado = ?) or (usuario_solicita = ? and usuario_solicitado = ?)"
+	if err := orm.Where(str, usuario, session.Usuario, session.Usuario, usuario).Find(&relaciones); err != nil {
+		w.WriteHeader(http.StatusInternalServerError)
+		return
+	}
+	if len(relaciones) == 1 && relaciones[0].EstadoRelacion == 0 && relaciones[0].UsuarioSolicita == usuario {
+		relaciones[0].EstadoRelacion = 1
+		models.Update(relaciones[0].Id, relaciones[0])
+		w.WriteHeader(http.StatusOK)
+	} else if len(relaciones) == 0 {
+		models.Add(models.Relacion{
+			UsuarioSolicita: session.Usuario,
+			UsuarioSolicitado: usuario,
+			EstadoRelacion: 0,
+		})
+		w.WriteHeader(http.StatusOK)
+	}
 }
