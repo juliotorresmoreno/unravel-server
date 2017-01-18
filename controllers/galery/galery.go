14bbf99bc93e1dd63eff77b1bc8ebf76650433d3
Julio Torres <jtorres990@gmail.com>
2017-01-18 00:04:42 -0500

    galery

--- controllers/galery/galery.go
+++ controllers/galery/galery.go
@@ -1,29 +1,39 @@
 package galery
 
 import (
+	"encoding/json"
+	"io"
+	"io/ioutil"
 	"net/http"
 	"os"
 	"strings"
-	"io/ioutil"
-	"encoding/json"
 
+	"../../config"
 	"../../helper"
 	"../../models"
 	"../../ws"
-	"../../config"
 )
 
 // Upload sube las imagenes
 func Upload(w http.ResponseWriter, r *http.Request, session *models.User, hub *ws.Hub) {
-	w.WriteHeader(http.StatusOK)
-
-	var galeria = r.PostFormValue("galery")
-	var name = r.PostFormValue("name")
-	var file = r.PostFormValue("file")
-
-	println(name, galeria)
-	println(len(file))
-	w.Write([]byte(file))
+	var galery = strings.Trim(r.FormValue("galery"), " ")
+	var galeria = config.PATH + "/" + session.Usuario + "/" + galery
+	f, _, err := r.FormFile("file")
+	if err != nil {
+		w.WriteHeader(http.StatusInternalServerError)
+		println(err.Error())
+		return
+	}
+	g, _ := os.Create(galeria + "/TempFile")
+	defer g.Close()
+	_, err = io.Copy(g, f)
+	if err != nil {
+		w.WriteHeader(http.StatusInternalServerError)
+		println(err.Error())
+		return
+	}
+	w.WriteHeader(http.StatusCreated)
+	w.Write([]byte("{\"success\": true}"))
 }
 
 // Create crea la nueva galeria
@@ -40,7 +50,7 @@ func Create(w http.ResponseWriter, r *http.Request, session *models.User, hub *w
 	}
 
 	var galeria = config.PATH + "/" + session.Usuario + "/" + nombre
-	if _, err:= os.Stat(galeria); err != nil {
+	if _, err := os.Stat(galeria); err != nil {
 		if err = os.MkdirAll(galeria, 0755); err != nil {
 			w.WriteHeader(http.StatusNotAcceptable)
 			w.Write([]byte("{\"success\": false}"))
@@ -49,12 +59,12 @@ func Create(w http.ResponseWriter, r *http.Request, session *models.User, hub *w
 	}
 
 	p, _ := os.Create(galeria + "/permiso")
-	p.Write([]byte(permiso))
 	defer p.Close()
+	p.Write([]byte(permiso))
 
 	d, _ := os.Create(galeria + "/descripcion")
-	d.Write([]byte(descripcion))
 	defer d.Close()
+	d.Write([]byte(descripcion))
 
 	w.WriteHeader(http.StatusCreated)
 	w.Write([]byte("{\"success\": true}"))
@@ -69,17 +79,17 @@ func Listar(w http.ResponseWriter, r *http.Request, session *models.User, hub *w
 	for i := 0; i < length; i++ {
 		permiso, _ := ioutil.ReadFile(usuario + "/" + files[i].Name() + "/permiso")
 		descripcion, _ := ioutil.ReadFile(usuario + "/" + files[i].Name() + "/descripcion")
-		list[i] = map[string]interface{} {
-			"name": files[i].Name(),
-			"permiso": string(permiso),
+		list[i] = map[string]interface{}{
+			"name":        files[i].Name(),
+			"permiso":     string(permiso),
 			"descripcion": string(descripcion),
 		}
 	}
 	w.Header().Set("Content-Type", "application/json")
 	w.WriteHeader(http.StatusOK)
-	respuesta, _ := json.Marshal(map[string]interface{} {
+	respuesta, _ := json.Marshal(map[string]interface{}{
 		"success": true,
-		"data": list,
+		"data":    list,
 	})
 	w.Write([]byte(respuesta))
 }
